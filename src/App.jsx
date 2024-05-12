2d2e7ac0c92b2f64027d49115ee173b748f0e4b5
Arafin Mridul <mridularafin@gmail.com>
2024-05-12 14:16:01 +0600

    deps set

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,34 @@
 import React from "react";
+import { Switch, Route, Link } from "react-router-dom";
+import { Layout, Typography, Space } from "antd";
+import Navbar from "./components/Navbar";
 
-const App = () => {
-    return <div>App</div>;
-};
+const App = () => (
+    <div className="app">
+        <div className="navbar">
+            <Navbar />
+        </div>
+        <div className="main">
+            <Layout>
+                <div className="routes"></div>
+            </Layout>
+            <div className="footer">
+                <Typography.Title
+                    level={5}
+                    style={{ color: "white", textAlign: "center" }}
+                >
+                    Copyright © 2024
+                    <Link to="/">Crypto Tracker</Link> <br />
+                    All Rights Reserved.
+                </Typography.Title>
+                <Space>
+                    <Link to="/">Home</Link>
+                    <Link to="/exchanges">Exchanges</Link>
+                    <Link to="/news">News</Link>
+                </Space>
+            </div>
+        </div>
+    </div>
+);
 
 export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React, { useState, useEffect } from "react";
+import { Button, Menu, Typography, Avatar } from "antd";
+import { Link } from "react-router-dom";
+import {
+    HomeOutlined,
+    MoneyCollectOutlined,
+    BulbOutlined,
+    FundOutlined,
+    MenuOutlined,
+} from "@ant-design/icons";
+
+import icon from "../images/cryptocurrency.png";
+
+const Navbar = () => {};
+
+export default Navbar;
