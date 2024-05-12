e444d9f3fa327a3672e97594b4c93c5d13527e75
Arafin Mridul <mridularafin@gmail.com>
2024-05-12 15:41:15 +0600

    routes components

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
 import React from "react";
-import { Switch, Route, Link } from "react-router-dom";
+import { Route, Link, Routes } from "react-router-dom";
 import { Layout, Typography, Space } from "antd";
 
-import { Navbar } from "./components";
+import {
+    Navbar,
+    Homepage,
+    Exchanges,
+    Cryptocurrencies,
+    CryptoDetails,
+    News,
+} from "./components";
 import "./App.css";
 
 const App = () => (
@@ -12,7 +19,27 @@ const App = () => (
         </div>
         <div className="main">
             <Layout>
-                <div className="routes"></div>
+                <div className="routes">
+                    <Routes>
+                        <Route exact path="/" element={<Homepage />} />
+                        <Route
+                            exact
+                            path="/exchanges"
+                            element={<Exchanges />}
+                        />
+                        <Route
+                            exact
+                            path="/cryptocurrencies"
+                            element={<Cryptocurrencies />}
+                        />
+                        <Route
+                            exact
+                            path="/crypto/:coinId"
+                            element={<CryptoDetails />}
+                        />
+                        <Route exact path="/news" element={<News />} />
+                    </Routes>
+                </div>
             </Layout>
             <div className="footer">
                 <Typography.Title
