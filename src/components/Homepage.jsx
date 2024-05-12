a7ef7e62e3e9974a8c2ddd81721cb6860adbf654
Arafin Mridul <mridularafin@gmail.com>
2024-05-12 18:22:31 +0600

    global stats shown

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -25,19 +25,34 @@ const Homepage = () => {
             </Title>
             <Row gutter={[32, 32]}>
                 <Col span={12}>
-                    <Statistic title="Total Cryptocurrencies" value="5" />
+                    <Statistic
+                        title="Total Cryptocurrencies"
+                        value={millify(globalStats.total)}
+                    />
                 </Col>
                 <Col span={12}>
-                    <Statistic title="Total Exchanges" value="5" />
+                    <Statistic
+                        title="Total Exchanges"
+                        value={millify(globalStats.totalExchanges)}
+                    />
                 </Col>
                 <Col span={12}>
-                    <Statistic title="Total Market Cap" value="5" />
+                    <Statistic
+                        title="Total Market Cap"
+                        value={`$${millify(globalStats.totalMarketCap)}`}
+                    />
                 </Col>
                 <Col span={12}>
-                    <Statistic title="Total 24h Volume" value="5" />
+                    <Statistic
+                        title="Total 24h Volume"
+                        value={`$${millify(globalStats.total24hVolume)}`}
+                    />
                 </Col>
                 <Col span={12}>
-                    <Statistic title="Total Markets" value="5" />
+                    <Statistic
+                        title="Total Markets"
+                        value={millify(globalStats.totalMarkets)}
+                    />
                 </Col>
             </Row>
         </>
