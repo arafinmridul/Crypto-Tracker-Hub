07d418d92dc19d85a5e6c4d7d4bc5006bf27bce6
Arafin Mridul <mridularafin@gmail.com>
2024-05-13 14:58:08 +0600

    data is being fetched

--- src/components/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies.jsx
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
+import millify from "millify";
+import { Link } from "react-router-dom";
+import { Card, Row, Col, Input } from "antd";
 
-const Cryptocurrencies = () => {
-    return <div>Cryptocurrencies</div>;
+import Loader from "./Loader";
+import { useGetCryptosQuery } from "../services/cryptoApi";
+
+const Cryptocurrencies = ({ simplified }) => {
+    const count = simplified ? 10 : 100;
+    const { data: cryptosList, isFetching } = useGetCryptosQuery(count);
+    const [cryptos, setCryptos] = useState(cryptosList?.data?.coins);
+
+    console.log(cryptos);
+
+    if (isFetching) return <Loader />;
+
+    return <></>;
 };
 
 export default Cryptocurrencies;
--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -1,6 +1,6 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const axios = require("axios");
+// const axios = require("axios");
 
 const baseUrl = "https://coinranking1.p.rapidapi.com";
 
