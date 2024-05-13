a2b4f25b9de2b4010b9951373cf545fd35dbebc9
Arafin Mridul <mridularafin@gmail.com>
2024-05-13 15:19:38 +0600

    all coins are showin g up

--- src/components/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import millify from "millify";
 import { Link } from "react-router-dom";
 import { Card, Row, Col, Input } from "antd";
@@ -11,6 +11,16 @@ const Cryptocurrencies = ({ simplified }) => {
     const { data: cryptosList, isFetching } = useGetCryptosQuery(count);
     const [cryptos, setCryptos] = useState(cryptosList?.data?.coins);
     const [searchTerm, setSearchTerm] = useState("");
+
+    useEffect(() => {
+        setCryptos(cryptosList?.data?.coins);
+
+        const filteredData = cryptosList?.data?.coins.filter((item) =>
+            item.name.toLowerCase().includes(searchTerm)
+        );
+
+        setCryptos(filteredData);
+    }, [cryptosList, searchTerm]);
 
     if (isFetching) return <Loader />;
 
